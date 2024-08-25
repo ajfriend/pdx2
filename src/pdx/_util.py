9acb5481b5817fb7071e398a7343e4f1efb3ead8
AJ Friend <ajfriend@gmail.com>
2024-08-25 12:56:38 -0700

    handle long filename checks

--- src/pdx/_util.py
+++ src/pdx/_util.py
@@ -1,9 +1,15 @@
 from pathlib import Path
 
-def _get_if_file(s: str) -> str:
-    path = Path(s)
 
-    if path.is_file():
+def _is_file(s: str) -> bool:
+    try:
+        return Path(s).is_file()
+    except OSError:
+        # catches `OSError: [Errno 63] File name too long`
+        return False
+
+def _get_if_file(s: str) -> str:
+    if _is_file(s):
         with open(s) as f:
             s = f.read()
 
