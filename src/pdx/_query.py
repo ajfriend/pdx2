b2cbb681a3de3d66d659be1b22bb5054d165b7fb
AJ Friend <ajfriend@gmail.com>
2024-08-24 17:22:37 -0700

    idea makes sense

--- src/pdx/_query.py
+++ src/pdx/_query.py
@@ -18,6 +18,45 @@ def sql(s, **dfs):
     return out
 
 
+class Database:
+    def __init__(self, **dfs):
+        con = duckdb.connect(
+            database = ':memory:',
+            config = {'enable_external_access': False},
+        )
+
+        for tbl_name, df in dfs.items():
+            df = pa.Table.from_pandas(df)
+            con.register(tbl_name, df)
+
+        self._con = con
+
+    def __repr__(self):
+        tables = yield_table_lines(self)
+
+        s = 'pdx.Database:\n'
+        for table in tables:
+            s += f'    {table}'
+
+        return s
+
+    def sql(self, s):
+        s = _get_if_file(s)
+        out = self._con.execute(s).df()
+        return out
+
+
+def yield_table_lines(db):
+    df = db.sql('show all tables')
+
+    for _, tbl in df.iterrows():
+        name = tbl['name']
+        columns = list(tbl['column_names'])
+        n = db.sql(f'select count() from {name}').asitem()
+
+        yield f'{name}: {n} x {columns}'
+
+
 # todo: can we do some fancier thing to specify the dicts?
 # https://docs.python.org/3/library/string.html#string.Formatter.vformat
 # s = """
